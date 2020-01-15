eccd465cad41dc3c9d66c94e12f7f3cdcfec0276
DeividasBa <55393172+DeividasBa@users.noreply.github.com>
2020-01-15 23:31:58 +0200

    About Us section animation

--- js/function.js
+++ js/function.js
@@ -117,6 +117,24 @@ function animation() {
 
 animation();
 /////////////Hero End/////////////
+////////////AboutMe Start///////////////
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 7000);
+}
+
+////////////AboutMe End///////////////
 
 /////////////Contacts/////////////
 
@@ -143,3 +161,8 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+
+////////////////
+
